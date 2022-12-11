42df60310fc402610cd8d3428b95857900d58ea5
Vikas Upadhyay <vikas.bhumca2006@gmail.com>
2022-12-12 00:44:51 +0530

    Update expression.go

--- pkg/expression/expression.go
+++ pkg/expression/expression.go
@@ -15,7 +15,7 @@
 package expression
 
 import (
-	api "github.com/capsule8/capsule8/api/v0"
+	api "github.com/VikasBhumca2006/capsule8/api/v0"
 )
 
 // FieldTypeMap is a mapping of types for field names/identifiers
