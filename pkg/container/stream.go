3d55ad411e285f75bbb14c86e8932647903d96a1
Vikas Upadhyay <vikas.bhumca2006@gmail.com>
2022-12-12 00:42:49 +0530

    Update stream.go

--- pkg/container/stream.go
+++ pkg/container/stream.go
@@ -14,7 +14,7 @@
 
 package container
 
-import "github.com/capsule8/capsule8/pkg/stream"
+import "github.com/VikasBhumca2006/capsule8/pkg/stream"
 
 // State represents the state of a container instance
 type State uint
