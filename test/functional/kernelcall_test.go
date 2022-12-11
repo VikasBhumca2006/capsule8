65dabfdc75a4a5b53b43323b4eeac714e4415a0e
Vikas Upadhyay <vikas.bhumca2006@gmail.com>
2022-12-12 01:07:52 +0530

    Update kernelcall_test.go

--- test/functional/kernelcall_test.go
+++ test/functional/kernelcall_test.go
@@ -17,8 +17,8 @@ package functional
 import (
 	"testing"
 
-	api "github.com/capsule8/capsule8/api/v0"
-	"github.com/capsule8/capsule8/pkg/expression"
+	api "github.com/VikasBhumca2006/capsule8/api/v0"
+	"github.com/VikasBhumca2006/capsule8/pkg/expression"
 	"github.com/golang/glog"
 )
 
